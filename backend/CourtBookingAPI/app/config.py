551a06684a37bb9486428eab4f633c7e94416d13
Unai <unai.devel@gmail.com>
2025-04-03 21:59:42 +0200

    variables bugs

--- backend/CourtBookingAPI/app/config.py
+++ backend/CourtBookingAPI/app/config.py
@@ -14,16 +14,15 @@ ENVIRONMENT VARIABLES WAY
 
 
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from pydantic import Basemodel
 
-class Settings(Basemodel):
+class Settings(BaseSettings):
     SECRET_KEY: str
     DATABASE_URL: str
 
-    class Config:
-        env_file = ".env"
+
+    model_config = SettingsConfigDict(env_file="/portfolio_project/backend/CourtBookingAPI/.env")
 
 
 settings = Settings()
-    
