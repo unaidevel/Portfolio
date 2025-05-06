ac484d6ef479b13e9b17e74102e03a1efde3d336
Unai <unai.devel@gmail.com>
2025-05-06 12:53:00 +0200

    seats endpoint added, session id get edited

--- backend/CinemaBookingAPI/app/models/__init__.py
+++ backend/CinemaBookingAPI/app/models/__init__.py
@@ -2,4 +2,4 @@ from .booking import Booking, BookingUpdate, BookingIn, BookingPublic
 from .session import Session, SessionIn, SessionPublic, SessionUpdate
 from .user import UserInDb, UserPublic, UserPassword, UserUpdate
 from .movie import Movie, MovieUpdate, MovieCreate, MoviePublic
-from .seats import Seats
+from .seats import Seat
--- backend/CinemaBookingAPI/app/routers/sessions.py
+++ backend/CinemaBookingAPI/app/routers/sessions.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, Depends, Query, status
 from typing import Annotated
-from app.models import Session, SessionPublic, SessionUpdate
-from app.auths.auth import SessionDep
+from app.models import Session, SessionPublic, SessionUpdate, Seat
+from app.auths.auth import SessionDep, get_current_user
 from app.auths.dependency import admin_only
 from sqlmodel import select
 from fastapi.exceptions import HTTPException
 from uuid import UUID
+from app.models.seats import generate_seats_for_session
+from collections import defaultdict
+
 
 session_router = APIRouter()
 
@@ -15,10 +18,17 @@ session_router = APIRouter()
 async def create_session(new_session: Session,
                          session: SessionDep, 
                          current_user: Annotated[str, Depends(admin_only)]):
-    session_existing = session.exec(select(Session).where)
-    session.add(new_session)
+    session_existing = session.exec(select(Session).where(Session.id == new_session.id)).first()
+    if session_existing:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Session already exists!')
+    
+    session.add(new_session)   #Create session before the seats, needed to have session_id to asign it afterwards
     session.commit()
     session.refresh(new_session)
+
+    seats = generate_seats_for_session(new_session.id)
+    session.add_all(seats)
+    session.commit()
     return new_session
 
 @session_router.get('/sessions', response_model=SessionPublic)
@@ -29,14 +39,30 @@ async def read_sessions(session: SessionDep, offset:int = 0, limit: Annotated[in
     return all_sessions
 
 @session_router.get('/sessions/{session_id}}', response_model=SessionPublic)
-async def read_session_by_id(session_id: str, session: SessionDep):
+async def read_session_by_id(session_id: UUID, session: SessionDep):
     film = session.get(Session, session_id)
     if not film:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Film not found')
     return film
 
+@session_router.get('/session/{session_id}/seats', response_model=Seat)
+async def read_seats_for_session(session_id: UUID, session: SessionDep, current_user:Annotated[str, Depends(get_current_user)]):
+    seats = session.exec(select(Seat).where(Seat.session_id==session_id)).all()
+    seats_per_row = defaultdict(list)
+    for seat in seats:
+        row = seat.seat_number[0]
+        number = int(seat.seat_number[1:])
+        seats_per_row[row].append({
+            "number": number,
+            "reserved": seat.is_reserved
+        })
+    return seats_per_row
+
+
+
+
 @session_router.patch('/sessions/{session_id}', response_model=SessionPublic)
-async def update_session_by_id(session_id:str, session_update: SessionUpdate, session: SessionDep, current_user: Annotated[str, Depends(admin_only)]):
+async def update_session_by_id(session_id:UUID, session_update: SessionUpdate, session: SessionDep, current_user: Annotated[str, Depends(admin_only)]):
     specific_session = session.get(Session, session_id)
     if not specific_session:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Session not found!')
@@ -50,7 +76,7 @@ async def update_session_by_id(session_id:str, session_update: SessionUpdate, se
 
 
 @session_router.delete('/sessions/{session_id}')
-async def delete_session(session_id: str, session: SessionDep, current_user: Annotated[str, Depends(admin_only)]):
+async def delete_session(session_id: UUID, session: SessionDep, current_user: Annotated[str, Depends(admin_only)]):
     specific_session = session.get(Session, session_id)
     session.delete(specific_session)
     session.commit()
